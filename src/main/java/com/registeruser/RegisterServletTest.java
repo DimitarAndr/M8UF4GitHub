cc4bae996a79edbc1ce4332adeb498d94f294bc6
Dimitar Andreev <cf17Dimiter.andreev@iesjoandaustria.org>
2019-05-03 18:12:31 +0200

    RegisterServletTest.java adding test

---
+++ src/main/java/com/registeruser/RegisterServletTest.java
@@ -0,0 +1,22 @@
+package com.registeruser;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegisterServletTest {
+
+    @org.junit.jupiter.api.Test
+    void doPost() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void checkUserNameDB() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void writeInDDBB() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addClassForName() {
+    }
+}
